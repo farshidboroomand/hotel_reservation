60b19af2a5a2b91e56c7473d2bf99149c1117818
Farshid Boroomand <farshid@luckytrip.co.uk>
2024-07-19 19:03:16 +0330

    WIP on users module

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/farshidboroomand/hotel_reservation/api"
+	"github.com/farshidboroomand/hotel_reservation/types"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dbUri = "mongodb://localhost:27017"
+const dbName = "hotel_reservation"
+const collectionName = "users"
+
 func main() {
-	listenAddr := flag.String("listenAddr", ":5000", "The Listen Address Of Api Srver")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbUri))
+	collection := client.Database(dbName).Collection(collectionName)
+	ctx := context.Background()
+	user := types.User{
+		FirstName: "farshid",
+		LastName:  "borooamnd",
+	}
+
+	res, err := collection.InsertOne(ctx, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(res)
+
+	listenAddr := flag.String("listenAddr", ":5000", "The Listen Address Of Api Server")
 	flag.Parse()
 
 	app := fiber.New()
